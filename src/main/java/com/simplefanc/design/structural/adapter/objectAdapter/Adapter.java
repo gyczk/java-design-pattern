ef2c122e49a516e5df45e801b28f97b1ad1ae05e
gyczk <GYczk320721>
2022-11-21 01:22:49 +0800

    fix

--- src/main/java/com/simplefanc/design/structural/adapter/objectAdapter/Adapter.java
+++ src/main/java/com/simplefanc/design/structural/adapter/objectAdapter/Adapter.java
@@ -5,7 +5,7 @@ package com.simplefanc.design.structural.adapter.objectAdapter;
  * 对象适配器
  */
 public class Adapter implements Target {
-    private Adaptee adaptee = new Adaptee();
+    private final Adaptee adaptee = new Adaptee();
 
     @Override
     public void request() {
